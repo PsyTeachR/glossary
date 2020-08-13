4320b3ab1017c80b334dbf27b6aa6e5ff7da7aea
debruine <debruine@gmail.com>
2020-08-13 10:16:05 +0100

    Update

---
+++ _setup.R
@@ -0,0 +1,8 @@
+new_entry <- function(term, dfn = "", open = FALSE) {
+  txt <- sprintf("## %s\n\n<dfn>%s</dfn>\n\n", term, dfn)
+  filename <- sprintf("terms/%s.Rmd", gsub("\\W+", "-", tolower(term)))
+  write(txt, filename)
+  if (open) browseURL(filename)
+}
+
+bookdown::render_book("index.Rmd")
