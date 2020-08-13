4bcc58e09fbf5c842f8cb8c536b8f6be0a659750
debruine <debruine@gmail.com>
2020-08-13 12:54:31 +0100

    New terms for CH1 msc-data-skills

--- _setup.R
+++ _setup.R
@@ -1,6 +1,10 @@
-new_entry <- function(term, dfn = "", open = FALSE) {
+new_entry <- function(term, dfn = "", open = FALSE, overwrite = FALSE) {
   txt <- sprintf("## %s\n\n<dfn>%s</dfn>\n\n", term, dfn)
   filename <- sprintf("terms/%s.Rmd", gsub("\\W+", "-", tolower(term)))
+  if (file.exists(filename) & !overwrite) {
+    stop(filename, " exists")
+  }
+  
   write(txt, filename)
   if (open) browseURL(filename)
 }
