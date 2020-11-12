7f040e85ae790f18c55024dd41fa65b141699159
debruine <debruine@gmail.com>
2020-11-12 22:34:18 +0000

    added null effect

---
+++ terms/null-effect.R
@@ -0,0 +1,5 @@
+## null effect
+
+<dfn>An outcome that does not show an otherwise expected effect.</dfn>
+
+A null effect could be a difference of 0 between two groups, or a chance value, such as 50% is a two-alternative forced choice task.
